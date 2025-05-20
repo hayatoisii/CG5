119c6a60927a4c90871673e68af88a7ecc1fecab
hayatoisii <k023g0022@g.neec.ac.jp>
2025-05-20 16:00:25 +0900

    無理

--- DirectXGame/Shader.cpp
+++ DirectXGame/Shader.cpp
@@ -1,17 +1,22 @@
 #include "Shader.h"
 #include <d3dcompiler.h>
 #include <cassert>
+#include <dxcapi.h>
+#pragma comment(lib, "d3dcompiler.lib")
 
-void Shader::Load(const std::wstring& filePath, const std::string& shaderModel) {
+void Shader::Load(const std::wstring& filePath, const std::wstring& shaderModel) {
 
 	ID3D10Blob* shaderBlob = nullptr;
 	ID3D10Blob* errorBlob = nullptr;
+
+	// wstring => string 文字列変換
+	std::string mbShaderModel = ConvertString(shaderModel);
 
 	HRESULT hr = D3DCompileFromFile(
 	    filePath.c_str(), // シェーダファイル名
 	    nullptr,
 	    D3D_COMPILE_STANDARD_FILE_INCLUDE,               // インクルード可能にする
-	    "main", shaderModel.c_str(),                     // エントリーポイント名、シェーダーモデル指定
+	    "main", mbShaderModel.c_str(),
 	    D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
 	    0, &shaderBlob, &errorBlob);
 
@@ -24,7 +29,93 @@ void Shader::Load(const std::wstring& filePath, const std::string& shaderModel) 
 	blob_ = shaderBlob;
 }
 
+// シェーダーファイルを読み込み、コンパイルする
+void Shader::LoadDxc(const std::wstring& filePath, const std::wstring& shaderModel) {
+	// DXC(DirectX Shader Compiler)を初期化
+	static IDxcUtils* dxcUtils = nullptr;
+	static IDxcCompiler3* dxcCompiler = nullptr;
+	static IDxcIncludeHandler* includeHandler = nullptr;
+
+	HRESULT hr;
+
+	if (dxcUtils == nullptr) {
+		hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
+		assert(SUCCEEDED(hr));
+	}
+
+	if (dxcCompiler == nullptr) {
+		hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
+		assert(SUCCEEDED(hr));
+	}
+
+	if (includeHandler == nullptr) {
+		hr = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
+		assert(SUCCEEDED(hr));
+	}
+
+	// 1. hlslファイルを読む
+	IDxcBlobEncoding* shaderSource = nullptr;
+	hr = dxcUtils->LoadFile(filePath.c_str(), nullptr, &shaderSource);
+	assert(SUCCEEDED(hr));
+
+	// 読み込んだファイルの内容をDxcBufferに設定する
+	DxcBuffer shaderSourceBuffer{};
+	shaderSourceBuffer.Ptr = shaderSource->GetBufferPointer();
+	shaderSourceBuffer.Size = shaderSource->GetBufferSize();
+	shaderSourceBuffer.Encoding = DXC_CP_UTF8;
+
+	// 2. Compilesuru
+	// Compileに必要なコンパイルオプションの準備
+	LPCWSTR arguments[] = {
+	    filePath.c_str(), 
+		L"-E", 
+		L"main", 
+		L"-T", 
+		shaderModel.c_str(), 
+		L"-Zi", 
+		L"-Qembed_debug", 
+		L"-Od", 
+		L"-Zpr",
+	};
+
+	// 実際にShaderをコンパイルする
+	IDxcResult* shaderResult = nullptr;
+	hr = dxcCompiler->Compile(
+	    &shaderSourceBuffer,        // 読み込んだファイル
+	    arguments,                  // コンパイルオプション
+	    _countof(arguments),        // コンパイルオプション
+	    includeHandler,             // includeが含まれた諸々
+	    IID_PPV_ARGS(&shaderResult) // コンパイル結果
+	);
+	// コンパイルエラーではなくdxcが起動できないなど致命的な状況
+	assert(SUCCEEDED(hr));
+
+	// 3. 警告・エラーが出ていないかを確認する
+	IDxcBlobUtf8* shaderError = nullptr;
+	IDxcBlobWide* nameBlob = nullptr;
+	shaderResult->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&shaderError), &nameBlob);
+	if (shaderError != nullptr && shaderError->GetStringLength() != 0) {
+		OutputDebugStringA(shaderError->GetStringPointer());
+		assert(false);
+	}
+
+	// 4. Compile結果を受け取る
+	IDxcBlob* shaderBlob = nullptr;
+	hr = shaderResult->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&shaderBlob), &nameBlob);
+	assert(SUCCEEDED(hr));
+
+	// もう使わないリソースを解放
+	shaderSource->Release();
+	shaderResult->Release();
+
+	// 実行用のバイナリを取っておく
+	dxcBlob_ = shaderBlob;
+	 
+}
+
 ID3DBlob* Shader::GetBlob() { return blob_; }
+
+IDxcBlob* Shader::GetDxcBlob() { return dxcBlob_; }
 
 Shader::Shader() {}
 
@@ -34,5 +125,8 @@ Shader::~Shader() {
 		blob_->Release();
 		blob_ = nullptr;
 	}
-
+	if (dxcBlob_ != nullptr) {
+		dxcBlob_->Release();
+		dxcBlob_ = nullptr;
+	}
 }
--- DirectXGame/TestPS.hlsl
+++ DirectXGame/TestPS.hlsl
@@ -1,11 +1,11 @@
 struct PixelShaderOutPut
 {
-    float4 color : SV_TARGET0;
+    float32_t4 color : SV_TARGET0;
 };
 
 PixelShaderOutPut main()
 {
     PixelShaderOutPut output;
-    output.color = float4(1.0f, 1.0f, 1.0f, 1.0f);
+    output.color = float32_t4(1.0f, 1.0f, 1.0f, 1.0f);
     return output;
 }
