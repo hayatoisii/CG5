f1024866ec1c5eda56a23dd2e559f0c5bb4ae2d0
hayatoisii <k023g0022@g.neec.ac.jp>
2025-07-03 11:01:18 +0900

    完成

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -58,15 +58,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		Vector2 texcoord;
 	};
 
-	/*/
-	// 頂点データの準備
-	VertexData vertices[] = {
-	    {0.0f,  0.5f,  0.0f, 1.0f}, // 左下
-	    {0.5f,  -0.5f, 0.0f, 1.0f}, // 上
-	    {-0.5f, -0.5f, 0.0f, 1.0f}, // 右下
-	};
-	/*/
-
 	// 真っ白
 	VertexData vertices[] = {
 	    {{-1.0f, 1.0f, 0.0f, 1.0f},  {0.0f, 0.0f}}, // 左下
